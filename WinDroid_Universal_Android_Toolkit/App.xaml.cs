121255da51f430fec6de4cd561886d179e8e6793
WindyCityRockr <WindyCityRockr@gmail.com>
2014-12-28 17:01:02 -0600

    Initial Commit
    Commit all the things!

---
+++ WinDroid_Universal_Android_Toolkit/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WinDroid_Universal_Android_Toolkit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
